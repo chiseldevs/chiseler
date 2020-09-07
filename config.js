e8fed9fdce92380a61d43f0b529e4e4ed382eb7f
Sokhibjon Orzikulov <genemators@gmail.com>
2020-09-08 00:22:29 +0500

    configuring wip

--- config.js
+++ config.js
@@ -1,122 +1,147 @@
-import type { InlineKeyboardMarkup } from "telegraf/typings/telegram-types";
-
-export type InlineKeyboard = InlineKeyboardMarkup["inline_keyboard"];
+// @ts-check
+'use strict';
 
 /**
- * String to be parsed by https://npmjs.com/millisecond,
- * or number of milliseconds.
+ * @typedef { import('./typings/config').Config } Config
+ * @typedef { import('./typings/config').InlineKeyboard } InlineKeyboard
  */
-type ms = number | string;
 
-export interface Config {
+/*
+ * Create `config.js` by running `cp example.config.js config.js`
+ * in the project folder, then edit it.
+ *
+ * Config file in JSON format (`config.json`) is also supported.
+ * For backwards compatibility, and because why not, it needs no extra code.
+ */
+
+/**
+ * Millisecond
+ * String to be parsed by https://npmjs.com/millisecond,
+ * or number of milliseconds. Pass 0 to remove immediately.
+ * @typedef {( number | string )} ms
+ */
+
+/**
+ * @type {Config}
+ */
+const config = {
+
 	/**
+	 * @type {!( number | string | (number|string)[] ) | undefined}
 	 * ID (number) or username (string) of master,
 	 * the person who can promote and demote admins,
 	 * and add the bot to groups.
 	 */
-	master: number | string | (number | string)[] | undefined;
+	master: process.env.MASTER,
 
 	/**
+	 * @type {!string | undefined}
 	 * Telegram Bot token obtained from https://t.me/BotFather.
 	 */
-	token: string | undefined;
+	token: process.env.BOT_TOKEN,
 
-	chats?: {
-		/**
-		 * Chat to log all admin actions to.
-		 */
-		adminLog?: number;
+
+	chats: {
 
 		/**
+		 * @type {(number | string | false ) | undefined}
 		 * Chat to send member join/leave notifications to.
 		 * Pass false to disable this feature.
 		 */
-		presenceLog?: number | string | false | undefined;
+		presenceLog: process.env.CONTROLLER,
 
 		/**
+		 * @type {(number | string | false) | undefined}
 		 * Chat to send report notifications to.
 		 * Pass false to disable this feature.
 		 */
-		report?: number | string | false | undefined;
-	};
+		report: process.env.CONTROLLER,
+	},
 
 	/**
+	 * @type {( 'all' | 'own' | 'none' )}
 	 * Which messages with commands should be deleted?
 	 * Defaults to 'own' -- don't delete commands meant for other bots.
 	 */
-	deleteCommands?: "all" | "own" | "none";
+	deleteCommands: 'own',
 
-	deleteCustom?: {
-		longerThan: number; // UTF-16 characters
-		after: ms;
-	};
+	deleteCustom: {
+		longerThan: 450, // UTF-16 characters
+		after: '20 minutes',
+	},
 
 	/**
+	 * @type {(ms | false)} Millisecond
 	 * Timeout before removing join and leave messages.
 	 * [Look at typedef above for details.]
 	 * Pass false to disable this feature.
 	 */
-	deleteJoinsAfter?: ms | false;
+	deleteJoinsAfter: '2 minutes',
 
 	/**
+	 * @type {(ms | { auto: (ms | false), manual: (ms | false) } | false)}
 	 * Timeout before removing auto-warn messages.
 	 * [Look at typedef above for details.]
 	 * Pass an object with { auto, manual } for more granular control
 	 * over which messages get deleted
 	 * Pass false to disable this feature.
 	 */
-	deleteWarnsAfter?: ms | { auto: ms | false; manual: ms | false } | false;
+	deleteWarnsAfter: false,
 
 	/**
+	 * @type {(ms | false)}
 	 * Timeout before removing ban messages.
 	 * [Look at typedef above for details.]
 	 * Pass false to disable this feature.
 	 */
-	deleteBansAfter?: ms | false;
+	deleteBansAfter: false,
 
 	/**
+	 * @type {string[]}
 	 * List of blacklisted domains.
 	 * Messages containing blacklisted domains will automatically be warned.
 	 * If the link is shortened, an attempt will be made to resolve it.
 	 * If resolved link is blacklisted, it will be warned for.
 	 */
-	blacklistedDomains?: string[];
+	blacklistedDomains: [],
 
 	/**
+	 * @type {( string[] | false )}
 	 * List of whitelisted links and usernames,
 	 * For channels and groups to stop warning users for them.
 	 * Pass false to whitelist all links and channels.
 	 */
-	excludeLinks?: string[] | false;
+	excludeLinks: [],
 
 	/**
+	 * @type {ms}
 	 * Don't count warns older than this value towards automatic ban.
 	 * [Look at typedef above for details.]
 	 */
-	expireWarnsAfter?: ms;
+	expireWarnsAfter: Infinity,
 
 	/**
+	 * @type {InlineKeyboard}
 	 * Inline keyboard to be added to reply to /groups.
 	 * We use it to display button opening our webpage.
 	 */
-	groupsInlineKeyboard?: InlineKeyboard;
+	groupsInlineKeyboard: [],
 
-	numberOfWarnsToBan: number;
+	numberOfWarnsToBan: 3,
 
 	/**
+	 * @type {string[]}
 	 * List of plugin names to be loaded.
 	 * See Readme in plugins directory for more details.
 	 */
-	plugins?: string[];
-
-	spamwatch?: {
-		token: string;
-		host?: string;
-	};
+	plugins: [],
 
 	/**
+	 * @type {InlineKeyboard}
 	 * Inline keyboard to be added to warn message.
 	 * We use it to display button showing our rules.
 	 */
-	warnInlineKeyboard?: InlineKeyboard;
-}
+	warnInlineKeyboard: [],
+};
+
+module.exports = Object.freeze(config);
--- handlers/commands/help.js
+++ handlers/commands/help.js
@@ -6,16 +6,12 @@ const message = `\
 Hey there!
 
 I'm an <b>administration</b> bot that helps you to keep \
-your <b>groups</b> safe from <b>spammers.</b>
+Chisel Devs <b>groups</b> safe from <b>spammers.</b>
 
 Send /commands to get the list of available commands.
 
 If you want to use me for your groups, \
-note that I'm more useful on a network of groups and \
-you also need to <b>setup a new bot.</b>
-
-So if you don't wish to self-host, @MissRose_bot \
-might be a better choice for you.
+contact with @genemator for further details.
 `;
 
 /** @param { import('../../typings/context').ExtendedContext } ctx */
@@ -26,7 +22,7 @@ const helpHandler = ({ chat, replyWithHTML }) => {
 		message,
 		Markup.inlineKeyboard([
 			Markup.urlButton('🛠 Setup a New Bot', homepage)
-		]).extra()
+		]).extra(),
 	);
 };
 
